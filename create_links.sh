6a288438dcdde98ab30340fff0c3ce95809ffdd0
Luca <luca.ant96@libero.it>
2020-04-30 20:13:45 +0200

    add packets

--- create_links.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WD=$(dirname $(realpath $0))
-source $WD/check_user.sh
-
-rm -r ~/.zshrc
-ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
-rm -r ~/.zprofile
-ln -s ~/dotfiles/zsh/zprofile ~/.zprofile
--- install_i3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WD=$(dirname $(realpath $0))
-source $WD/check_user.sh
-
-
-sudo pacman -S i3 xorg-xinit xorg-server dmenu xterm scrot rofi xautolock nitrogen xorg-xinput
-
